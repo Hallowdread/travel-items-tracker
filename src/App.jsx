981f8591abc4e87e648e1b917ad9adb3723e909f
Hallowdread <hallowpahe@gmail.com>
2024-11-21 01:00:31 +0100

    added Form component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="app">
       <Logo />
+      <Form />
     </div>
   );
 }
@@ -13,5 +14,21 @@ const Logo = () => {
     <header>
       <h1>🌴FAR AWAY💼</h1>
     </header>
+  );
+};
+
+const Form = () => {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your😍trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Items..." />
+    </form>
   );
 };
