2ec80ca43190b4f836ecf2fb53e887e54a1bb7af
Hallowdread <hallowpahe@gmail.com>
2024-11-21 01:25:42 +0100

    added PackingList component and some comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import PropTypes from "prop-types";
 import "./index.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 3, packed: false },
+];
 
 export default function App() {
   return (
     <div className="app">
       <Logo />
       <Form />
+      <PackingList />
     </div>
   );
 }
 
+//? List Component
 const Logo = () => {
   return (
     <header>
@@ -17,6 +26,7 @@ const Logo = () => {
   );
 };
 
+//? Form Component
 const Form = () => {
   return (
     <form className="add-form">
@@ -31,4 +41,39 @@ const Form = () => {
       <input type="text" placeholder="Items..." />
     </form>
   );
+};
+
+//? PackingList Component
+const PackingList = () => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <List key={item.id} itemObj={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+//? List Component
+const List = ({ itemObj }) => {
+  return (
+    <li>
+      <span style={itemObj.packed ? { textDecoration: "line-through" } : {}}>
+        {itemObj.quantity} {itemObj.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
+//* Props Validation for List
+List.propTypes = {
+  itemObj: PropTypes.shape({
+    description: PropTypes.string.isRequired,
+    id: PropTypes.number.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    packed: PropTypes.bool,
+  }).isRequired,
 };
